abdcb1ee6179aba50476f78e8eb4039337512ff4
Zaycev Andrey <mr_zaycev@mail.ru>
2022-09-29 21:46:33 +0300

    начало бота

---
+++ botbrain.py
@@ -0,0 +1,28 @@
+import requests
+import telebot
+
+# @bot.message_handler(content_types=['text'])
+# def get_text_messages(message):
+#     print('ПРОСТО МЕСЕДЖ', message, end='\n')
+#     print("МЕСЕДЖ-ТХТ", message.text, end='\n')
+#     print("ЛОУВЕР", message.text.lower, end='\n')
+#     if message.text.lower == "привет":
+#
+#         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+#     elif message.text == "/help":
+#         bot.send_message(message.from_user.id, "Напиши привет")
+#     else:
+#         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+#
+#
+# bot.polling(none_stop=True, interval=0)
+print('Helo word')
+print('Helo word')
+print('Helo word')
+print('Helo word')
+print('Helo word')
+print('Helo word')
+print('Helo word')
+print('Helo word')
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+# import requests
+# import telebot
+
+import botbrain
+
+# with open('token.txt', 'r') as file:
+#     token = file.read()
+# print(token)
+
+# bot = telebot.TeleBot(token)
+
+
