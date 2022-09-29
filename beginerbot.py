33f4795cdd1a7c8edc48a8d0bb47737786136f2c
Zaycev Andrey <mr_zaycev@mail.ru>
2022-09-30 00:05:29 +0300

    начало бота

--- beginerbot.py
+++ beginerbot.py
@@ -2,7 +2,19 @@ import botbrain
 
 bot = botbrain.bot
 
+discription = "Это бот агенства Too Easy Travel. Он поможет Вам подобрать отель по заданным параметрам"
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,"Привет")
+    bot.send_message(message.from_user.id, 'Привет')
+    bot.send_message(message.from_user.id, discription)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == "привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
--- botbrain.py
+++ botbrain.py
@@ -1,21 +1,12 @@
-# import requests
 import telebot
+import beginerbot
 
 with open('token.txt', 'r') as file:
     token = file.read()
 
 bot = telebot.TeleBot(token)
 
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower == "привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
+beginerbot.start_message(message=None)
 
 bot.polling(none_stop=True, interval=0)
 
